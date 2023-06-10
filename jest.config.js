98849392fc4e77c5c68bc06010dbf68f2afa88a0
ISilviu <ifrim.silviu2@gmail.com>
2023-06-10 14:52:17 +0300

    Add extended matchers.

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,10 @@ const createJestConfig = nextJest({
   dir: "./",
 });
 
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-module.exports = createJestConfig();
+module.exports = createJestConfig(customJestConfig);
